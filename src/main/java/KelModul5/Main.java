85fe87cc4bde46c6bf6e30b07e315305c134c742
MuhammadNaufalAsh-shiddiq <168943033+MuhammadNaufalAsh-shiddiq@users.noreply.github.com>
2024-05-06 04:48:24 +0700

    No 5 Modul 5
    No 5 Modul 5

---
+++ src/main/java/KelModul5/Main.java
@@ -0,0 +1,27 @@
+
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan sebuah angka bulat positif: ");
+        int angka = input.nextInt();
+        
+        if (angka > 0) {
+            for (int i = 1; i <= angka; i++) {
+                System.out.print(i + " ");
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Mohon masukkan angka bulat positif.");
+        }
+        
+        input.close();
+    }
+}
