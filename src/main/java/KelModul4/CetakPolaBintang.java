14d2bcb63b875eff043600379bf6e4d509cbcccd
MuhammadNaufalAsh-shiddiq <168943033+MuhammadNaufalAsh-shiddiq@users.noreply.github.com>
2024-05-06 04:30:15 +0700

    No 4 Modul 4
    No 4 Modul 4

--- src/main/java/KelModul4/CetakPolaBintang.java
+++ src/main/java/KelModul4/CetakPolaBintang.java
@@ -1,21 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package KelModul4;
+package cetakpolabintang;
 
-/**
- *
- * @author Lenovo
- */
 public class CetakPolaBintang {
-     public static void main (String[] args){
+    public static void main(String[] args) {
         int n = 5;
-        for (int i = n; i >= 1; i--) {
-         for (int j = 1; j <= i; j++) {
-             System.out.print("* ");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
+    
 }
