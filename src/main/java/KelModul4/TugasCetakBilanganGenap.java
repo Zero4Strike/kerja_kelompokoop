3c7c63a519f05cbf806b92adffd845605495c0b0
MuhammadNaufalAsh-shiddiq <168943033+MuhammadNaufalAsh-shiddiq@users.noreply.github.com>
2024-05-06 04:27:54 +0700

    No 1
    No 1 Modul 4

---
+++ src/main/java/KelModul4/TugasCetakBilanganGenap.java
@@ -0,0 +1,17 @@
+
+package tugascetakbilangangenap;
+
+public class TugasCetakBilanganGenap {
+
+    public static void main(String[] args) {
+        
+        int n = 20;
+        for (int i = 1; i <= n; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            } 
+        }
+
+    }
+    
+}
